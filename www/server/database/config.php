220166624b06802c773012648f4519f8df3dea75
AldwynN <AldwynN@users.noreply.github.com>
2019-05-06 23:51:00 +0200

    Création du repository git

---
+++ www/server/database/config.php
@@ -0,0 +1,8 @@
+<?php
+
+DEFINE('DB_USER', '');
+DEFINE('DB_PASSWORD', '');
+DEFINE('DB_PORT', '3306');
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_NAME', '');
+DEFINE('DB_TYPE', 'mysql');
---
+++ www/server/database/database.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once 'config.php';
+
+/**
+ * Retourne un objet PDO connecté à la base de données
+ * @return \PDO
+ */
+class Database
+{
+
+    private static $pdoInstance;
+
+    /**
+     * @brief   Class Constructor - Créer une nouvelle connextion à la database si la connexion n'existe pas
+     *          On la met en privé pour que personne puisse créer une nouvelle instance via ' = new KDatabase();'
+     */
+    private function __construct()
+    { }
+
+    /**
+     * @brief   Comme le constructeur, on rend __clone privé pour que personne ne puisse cloner l'instance
+     */
+    private function __clone()
+    { }
+
+    /**
+     * @brief   Retourne l'instance de la Database ou créer une connexion initiale
+     * @return $objInstance;
+     */
+    public static function getInstance()
+    {
+        if (self::$pdoInstance == null) {
+            try {
+                $dsn = DB_TYPE . ':host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8;';
+                self::$pdoInstance = new PDO($dsn, DB_USER, DB_PASSWORD, null);
+                self::$pdoInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "EDatabase Error: " . $e->getMessage();
+            }
+        }
+        return self::$pdoInstance;
+    }
+
+    # end method
+    /**
+     * @brief   Passes on any static calls to this class onto the singleton PDO instance
+     * @param   $chrMethod      The method to call
+     * @param   $arrArguments   The method's parameters
+     * @return  $mix            The method's return value
+     */
+
+    final public static function __callStatic($chrMethod, $arrArguments)
+    {
+        $pdo = self::getInstance();
+        return call_user_func_array(array($pdo, $chrMethod), $arrArguments);
+    }
+
+    # end method
+}
+ 
